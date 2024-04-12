364a3bba5f9a8f763f9fb0300f786d0c3621271b
Taeyoung Won <hexter1994@gmail.com>
2024-04-12 16:16:00 +0900

    #4 add: 코멘트 관련 요청 모델 추가

---
+++ KeyCat/Data/Network/Request/CreateCommentRequest.swift
@@ -0,0 +1,10 @@
+//
+//  CreateCommentRequest.swift
+//  KeyCat
+//
+//  Created by 원태영 on 4/12/24.
+//
+
+struct CreateCommentRequest: HTTPRequestBody {
+  let content: String
+}
---
+++ KeyCat/Data/Network/Request/UpdateCommentRequest.swift
@@ -0,0 +1,10 @@
+//
+//  UpdateCommentRequest.swift
+//  KeyCat
+//
+//  Created by 원태영 on 4/12/24.
+//
+
+struct UpdateCommentRequest: HTTPRequestBody {
+  let content: String
+}
