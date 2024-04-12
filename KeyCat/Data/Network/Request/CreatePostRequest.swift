dce1770c598c3a6a27a60a7a31bfd29108b60451
Taeyoung Won <hexter1994@gmail.com>
2024-04-12 16:13:40 +0900

    #4 add: 게시물 관련 요청 모델 추가

---
+++ KeyCat/Data/Network/Request/CreatePostRequest.swift
@@ -0,0 +1,18 @@
+//
+//  CreatePostRequest.swift
+//  KeyCat
+//
+//  Created by 원태영 on 4/12/24.
+//
+
+struct CreatePostRequest: HTTPRequestBody {
+  let title: String?
+  let content: String?
+  let content1: String?
+  let content2: String?
+  let content3: String?
+  let content4: String?
+  let content5: String?
+  let product_id: String?
+  let files: [String]?
+}
