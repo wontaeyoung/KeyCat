75c066347464dbee4b9ccc5b448388748d80c603
Taeyoung Won <hexter1994@gmail.com>
2024-04-13 20:25:25 +0900

    #4 feat: 키보드 외관 정보 모델 정의

---
+++ KeyCat/Domain/Entity/Post/Keyboard/KeyboardAppearanceInfo.swift
@@ -0,0 +1,91 @@
+//
+//  KeyboardAppearanceInfo.swift
+//  KeyCat
+//
+//  Created by 원태영 on 4/13/24.
+//
+
+// MARK: - 키보드 외관
+struct KeyboardAppearanceInfo {
+  let ratio: LayoutRatio
+  let design: KeyboardDesign
+  let material: Material
+  let size: Size
+  
+  enum LayoutRatio: Int, SelectionExpressible {
+    case twenty
+    case forty
+    case sixty
+    case sixtyFive
+    case seventyFive
+    case eighty
+    case ninetySix
+    case hundred
+    case other
+    
+    var name: String {
+      switch self {
+        case .twenty: "20%"
+        case .forty: "40%"
+        case .sixty: "60%"
+        case .sixtyFive: "65%"
+        case .seventyFive: "75%"
+        case .eighty: "80%"
+        case .ninetySix: "96%"
+        case .hundred: "100%"
+        case .other: "기타"
+      }
+    }
+    
+    static var title: String {
+      return "배열"
+    }
+  }
+
+  enum KeyboardDesign: Int, SelectionExpressible {
+    case standard
+    case alice
+    case split
+    case oneHanded
+    
+    var name: String {
+      switch self {
+        case .standard: "일반"
+        case .alice: "인체공학"
+        case .split: "스플릿"
+        case .oneHanded: "한 손"
+      }
+    }
+    
+    static var title: String {
+      return "디자인"
+    }
+  }
+
+  enum Material: Int, SelectionExpressible {
+    case plastic
+    case aluminum
+    case plasticAluminum
+    case other
+    
+    var name: String {
+      switch self {
+        case .plastic: "플라스틱"
+        case .aluminum: "알루미늄"
+        case .plasticAluminum: "플라스틱/알루미늄"
+        case .other: "기타"
+      }
+    }
+    
+    static var title: String {
+      return "하우징"
+    }
+  }
+
+  struct Size {
+    let width: Double
+    let height: Double
+    let depth: Double
+    let weight: Double
+  }
+}
