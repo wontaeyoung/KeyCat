4ee1246732c6416f8c9b98cbf4f7a957bc0d45b5
Taeyoung Won <hexter1994@gmail.com>
2024-04-14 13:17:29 +0900

    #4 add: 커머스 게시글 가격 정보 모델 추가

---
+++ KeyCat/Domain/Entity/Post/Commerce/CommercialPrice.swift
@@ -0,0 +1,15 @@
+//
+//  CommercialPrice.swift
+//  KeyCat
+//
+//  Created by 원태영 on 4/14/24.
+//
+
+import Foundation
+
+struct CommercialPrice {
+  let regularPrice: Int
+  let couponPrice: Int
+  let discountPrice: Int
+  let discountExpiryDate: Date?
+}
