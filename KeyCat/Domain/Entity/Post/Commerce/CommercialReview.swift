b8290a747ed9ebdbd2a653a6508ac5d5ecd6b1ea
Taeyoung Won <hexter1994@gmail.com>
2024-04-14 21:07:54 +0900

    #4 add: 리뷰 모델 추가

---
+++ KeyCat/Domain/Entity/Post/Commerce/CommercialReview.swift
@@ -0,0 +1,30 @@
+//
+//  Review.swift
+//  KeyCat
+//
+//  Created by 원태영 on 4/14/24.
+//
+
+import Foundation
+
+struct CommercialReview: Entity {
+  
+  let reviewID: String
+  let content: String
+  let rating: Rating
+  let createdAt: Date
+  let creator: User
+  
+  enum Rating: Int, CaseIterable {
+    case one = 1
+    case two
+    case three
+    case four
+    case five
+    
+    var value: Int {
+      return self.rawValue
+    }
+  }
+}
+
