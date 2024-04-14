8c76c043d0576a427b23c31c162e16846b1eac9c
Taeyoung Won <hexter1994@gmail.com>
2024-04-14 21:09:39 +0900

    #4 feat: 커머스 게시글 모델 정의

---
+++ KeyCat/Domain/Entity/Post/Commerce/CommercialPost.swift
@@ -0,0 +1,29 @@
+//
+//  CommercialPost.swift
+//  KeyCat
+//
+//  Created by 원태영 on 4/14/24.
+//
+
+import Foundation
+
+struct CommercialPost: Entity {
+  
+  let postID: String
+  let postType: PostType
+  let title: String
+  let content: String
+  let keyboard: Keyboard
+  let price: CommercialPrice
+  let delivery: DeliveryInfo
+  let createdAt: Date
+  let creator: User
+  let files: [URLString]
+  let likes: [UserID]
+  let hashTags: [Hashtag]
+  let reviews: [CommercialReview]
+  
+  var productImagesURL: [URL?] {
+    return files.map { URL(string: $0) }
+  }
+}
