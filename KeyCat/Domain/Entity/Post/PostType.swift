875960f33807fe9b265adc923d47909484ab7ebf
Taeyoung Won <hexter1994@gmail.com>
2024-04-14 21:08:45 +0900

    #4 feat: PostType 정의

---
+++ KeyCat/Domain/Entity/Post/PostType.swift
@@ -0,0 +1,17 @@
+//
+//  PostType.swift
+//  KeyCat
+//
+//  Created by 원태영 on 4/14/24.
+//
+
+enum PostType: String {
+  
+  case keycat_commercialProduct
+  case keycat_secondhandTrade
+  case keycat_groupBuy
+  
+  var productID: String {
+    return self.rawValue
+  }
+}
