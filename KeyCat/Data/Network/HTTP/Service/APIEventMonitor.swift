6204efb941011adae94e92c783fce166d4e0d238
Taeyoung Won <hexter1994@gmail.com>
2024-04-16 19:47:07 +0900

    #3 fix: 에러 수신 생명주기 로그 메서드 추가

--- KeyCat/Data/Network/HTTP/Service/APIEventMonitor.swift
+++ KeyCat/Data/Network/HTTP/Service/APIEventMonitor.swift
@@ -40,17 +40,17 @@ final class APIEventMonitor: EventMonitor {
 """
     LogManager.shared.log(with: message, to: .network, level: .info)
   }
-
+  
   // URLRequest 생성 -> 네트워크 요청 시작 직전
   func request(_ request: Request, didCreateURLRequest urlRequest: URLRequest) { }
-
+  
   // URLSessionTask 생성 직후
   // 세션 작업 추적 혹은 관련 데이터 로깅
   func request(_ request: Request, didCreateTask task: URLSessionTask) { }
-
+  
   // 요청 작업 성공 혹은 취소 시점
   func requestDidFinish(_ request: Request) { }
-
+  
   // 요청 후 응답 완료
   // 성공 여부에 상관없이 호출
   func request(_ request: Request, didCompleteTask task: URLSessionTask, with error: AFError?) {
@@ -58,6 +58,7 @@ final class APIEventMonitor: EventMonitor {
       return
     }
     
+    let errorMessage = error?.localizedDescription ?? "에러 없음"
     let message = """
 {응답 완료}
 
@@ -67,11 +68,23 @@ final class APIEventMonitor: EventMonitor {
 [헤더정보]
 \(httpResponse.headers.description)
 
-[에러 메세지]
-\(error?.localizedDescription ?? "에러 없음")
-
 ---
 """
     LogManager.shared.log(with: message, to: .network, level: .info)
+  }
+  
+  func request<Value>(_ request: DataRequest, didParseResponse response: DataResponse<Value, AFError>) {
+    
+    guard let error = response.error, let data = response.data else { return }
+    
+    let message = """
+{파싱 에러}
+[에러 메세지]
+\(error)
+
+[응답 Json]
+\(data.toPrettyJsonString)
+"""
+    LogManager.shared.log(with: message, to: .network, level: .error)
   }
 }
