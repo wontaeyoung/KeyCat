bb3d7c825b13d4f2f903a53b25d823ebebbb36b0
Taeyoung Won <hexter1994@gmail.com>
2024-04-16 16:55:33 +0900

    #3 add: KCHeader 열거형 정의

---
+++ KeyCat/Data/Network/HTTP/Communication/Header/KCHeader.swift
@@ -0,0 +1,17 @@
+//
+//  KCHeader.swift
+//  KeyCat
+//
+//  Created by 원태영 on 4/16/24.
+//
+
+enum KCHeader {
+  enum Key {
+    static let contentType = "Content-Type"
+    static let sesacKey = "SesacKey"
+  }
+  
+  enum Value {
+    static let applicationJson = "application/json"
+  }
+}
