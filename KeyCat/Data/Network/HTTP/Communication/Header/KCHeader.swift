e80fe2c061ba9e398ab2117acdd4d8ef48bfdc18
Taeyoung Won <hexter1994@gmail.com>
2024-04-16 21:33:50 +0900

    #3 fix: 토큰 키 추가

--- KeyCat/Data/Network/HTTP/Communication/Header/KCHeader.swift
+++ KeyCat/Data/Network/HTTP/Communication/Header/KCHeader.swift
@@ -9,9 +9,12 @@ enum KCHeader {
   enum Key {
     static let contentType = "Content-Type"
     static let sesacKey = "SesacKey"
+    static let authorization = "Authorization"
+    static let refresh = "Refresh"
   }
   
   enum Value {
     static let applicationJson = "application/json"
+    static let multipartFormData = "multipart/form-data"
   }
 }
