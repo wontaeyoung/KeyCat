2bf38f3661741588edb6f49f665cdc6e1752c6c9
Taeyoung Won <hexter1994@gmail.com>
2024-04-18 16:12:27 +0900

    #3 fix: 내 프로필 수정 라우팅 추가

--- KeyCat/Data/Network/HTTP/Communication/Router/UserRouter.swift
+++ KeyCat/Data/Network/HTTP/Communication/Router/UserRouter.swift
@@ -13,6 +13,7 @@ enum UserRouter: Router {
   case follow(userID: Entity.UserID)
   case unfollow(userID: Entity.UserID)
   case myProfileFetch
+  case myProfileUpdate(request: UpdateMyProfileRequest)
   
   var method: HTTPMethod {
     switch self {
@@ -24,6 +25,9 @@ enum UserRouter: Router {
         
       case .myProfileFetch:
         return .get
+        
+      case .myProfileUpdate:
+        return .put
     }
   }
   
@@ -35,7 +39,7 @@ enum UserRouter: Router {
       case .unfollow(let userID):
         return "/follow/\(userID)"
         
-      case .myProfileFetch:
+      case .myProfileFetch, .myProfileUpdate:
         return "/users/me/profile"
     }
   }
@@ -46,12 +50,18 @@ enum UserRouter: Router {
         return [
           HTTPHeader(name: KCHeader.Key.authorization, value: KCHeader.Value.accessToken)
         ]
+        
+      case .myProfileUpdate:
+        return [
+          HTTPHeader(name: KCHeader.Key.authorization, value: KCHeader.Value.accessToken),
+          HTTPHeader(name: KCHeader.Key.contentType, value: KCHeader.Value.multipartFormData),
+        ]
     }
   }
   
   var parameters: Parameters? {
     switch self {
-      case .follow, .unfollow, .myProfileFetch:
+      case .follow, .unfollow, .myProfileFetch, .myProfileUpdate:
         return nil
     }
   }
@@ -60,6 +70,19 @@ enum UserRouter: Router {
     switch self {
       case .follow, .unfollow, .myProfileFetch:
         return nil
+        
+      case .myProfileUpdate(let request):
+        return requestToBody(request)
     }
+  }
+}
+
+extension UserRouter: URLConvertible {
+  func asURL() throws -> URL {
+    guard let url = try asURLRequest().url else {
+      throw HTTPError.invalidURL
+    }
+    
+    return url
   }
 }
