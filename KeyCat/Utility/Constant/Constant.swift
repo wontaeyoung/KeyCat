fe7f38c926bf5a1930177f56ec24e52a7aa5d0e9
Taeyoung Won <hexter1994@gmail.com>
2024-04-19 20:06:25 +0900

    #3 add: 리터럴 관리 Constant 열거형 정의

---
+++ KeyCat/Utility/Constant/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  KeyCat
+//
+//  Created by 원태영 on 4/19/24.
+//
+
+enum Constant {
+  
+  enum Label {
+    
+    static let appName: String = "KeyCat"
+    static let signUpInfo: String = "아직 계정이 없으신가요?"
+  }
+  
+  enum Button {
+    
+    static let signIn: String = "로그인"
+    static let signUp: String = "회원가입"
+  }
+}
