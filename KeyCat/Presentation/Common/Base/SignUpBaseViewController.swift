862062ae751b64c2dcfe595efd152479338147db
Taeyoung Won <hexter1994@gmail.com>
2024-04-20 00:38:05 +0900

    #3 add: 회원가입 화면 Base ViewController 클래스 추가

---
+++ KeyCat/Presentation/Common/Base/SignUpBaseViewController.swift
@@ -0,0 +1,91 @@
+//
+//  SignUpBaseViewController.swift
+//  KeyCat
+//
+//  Created by 원태영 on 4/19/24.
+//
+
+import UIKit
+import RxSwift
+
+class SignUpBaseViewController: RxBaseViewController {
+  
+  let inputInfoTitleLabel = KCLabel(style: .mainInfoTitle)
+  private let nextButton = KCButton(style: .primary, title: Constant.Button.duplicateCheck)
+  
+  private let bottomBufferHeight: CGFloat = 20
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    setLayout()
+    bindKeyboard()
+    bindEndEditing()
+  }
+  
+  init(inputInfoTitle: String) {
+    super.init()
+    
+    inputInfoTitleLabel.text = inputInfoTitle
+  }
+  
+  private func setLayout() {
+    view.addSubviews(inputInfoTitleLabel, nextButton)
+    
+    inputInfoTitleLabel.snp.makeConstraints { make in
+      make.top.equalTo(view.safeAreaLayoutGuide)
+      make.horizontalEdges.equalTo(view).inset(20)
+    }
+    
+    nextButton.snp.makeConstraints { make in
+      make.horizontalEdges.equalTo(view).inset(20)
+      make.bottom.equalTo(view.safeAreaLayoutGuide).inset(bottomBufferHeight)
+    }
+  }
+  
+  private func bindKeyboard() {
+    // 키보드 나타남 감지
+    NotificationCenter.default.rx.notification(UIResponder.keyboardWillShowNotification)
+      .map {
+        guard let height = ($0.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.height else {
+          return 0
+        }
+        
+        return height
+      }
+      .bind(with: self) { owner, height in
+        owner.updateButtonPosition(keyboardHeight: height)
+      }
+      .disposed(by: disposeBag)
+    
+    // 키보드 사라짐 감지
+    NotificationCenter.default.rx.notification(UIResponder.keyboardWillHideNotification)
+      .bind(with: self) { owner, _ in
+        owner.updateButtonPosition(keyboardHeight: 0)
+      }
+      .disposed(by: disposeBag)
+  }
+  
+  private func bindEndEditing() {
+    let tap = UITapGestureRecognizer()
+    view.addGestureRecognizer(tap)
+    
+    tap.rx.event
+      .bind(with: self) { owner, _ in
+        owner.view.endEditing(true)
+      }
+      .disposed(by: disposeBag)
+  }
+  
+  private func updateButtonPosition(keyboardHeight: CGFloat) {
+    let updateHeight = keyboardHeight + bottomBufferHeight
+    
+    UIView.animate(withDuration: 0.3) {
+      self.nextButton.snp.updateConstraints { make in
+        make.bottom.equalTo(self.view.safeAreaLayoutGuide).offset(-updateHeight)
+      }
+      
+      self.view.layoutIfNeeded()
+    }
+  }
+}
