be167cbb3d72492de73103dec8e2cd3a2b93bb7f
Taeyoung Won <hexter1994@gmail.com>
2024-04-21 18:56:15 +0900

    #7 fix: 이메일 관련 문자열 추가

--- KeyCat/Utility/Constant/Constant.swift
+++ KeyCat/Utility/Constant/Constant.swift
@@ -12,6 +12,8 @@ enum Constant {
     static let appName: String = "KeyCat"
     static let signUpInfo: String = "아직 계정이 없으신가요?"
     static let inputEmailInfo: String = "이메일을 알려주세요!"
+    static let duplicatedEmailInfo: String = "이미 가입된 이메일이에요."
+    static let avaliableEmailInfo: String = "사용 가능한 이메일이에요."
   }
   
   enum Button {
@@ -19,6 +21,7 @@ enum Constant {
     static let signIn: String = "로그인"
     static let signUp: String = "회원가입"
     static let duplicateCheck: String = "중복 확인"
+    static let duplicateCheckInfo: String = "중복 확인이 필요해요!"
     static let next: String = "다음으로"
   }
 }
