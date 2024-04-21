f5e6f2e4e9f6adffbd102cc5e66c276244b3c29b
Taeyoung Won <hexter1994@gmail.com>
2024-04-22 02:15:48 +0900

    #7 feat: 이메일, 중복검사 이벤트 바인딩

--- KeyCat/Presentation/Main/Auth/SignUp/SignUpEmailViewController.swift
+++ KeyCat/Presentation/Main/Auth/SignUp/SignUpEmailViewController.swift
@@ -15,7 +15,10 @@ final class SignUpEmailViewController: SignUpBaseViewController, ViewModelContro
   
   // MARK: - UI
   private let emailField = SignUpInputField(inputInformation: .email)
+  
   private let duplicateCheckButton = KCButton(style: .plain, title: Constant.Button.duplicateCheck)
+  
+  private let duplicateCheckResultInfoLabel = KCLabel(style: .caption, alignment: .center)
   
   // MARK: - Property
   let viewModel: SignUpViewModel
@@ -33,6 +36,7 @@ final class SignUpEmailViewController: SignUpBaseViewController, ViewModelContro
     
     view.addSubviews(
       emailField,
+      duplicateCheckResultInfoLabel,
       duplicateCheckButton
     )
   }
@@ -43,6 +47,12 @@ final class SignUpEmailViewController: SignUpBaseViewController, ViewModelContro
     emailField.snp.makeConstraints { make in
       make.top.equalTo(inputInfoTitleLabel.snp.bottom)
       make.leading.equalTo(view).inset(20)
+    }
+    
+    duplicateCheckResultInfoLabel.snp.makeConstraints { make in
+      make.horizontalEdges.equalTo(view).inset(20)
+      make.bottom.equalTo(nextButton.snp.top).offset(-10)
+      make.height.equalTo(20)
     }
     
     duplicateCheckButton.snp.makeConstraints { make in
@@ -58,11 +68,64 @@ final class SignUpEmailViewController: SignUpBaseViewController, ViewModelContro
   }
   
   override func bind() {
+    let input = SignUpViewModel.Input(
+      email: .init(),
+      duplicateCheckEvent: .init()
+    )
     
+    let output = viewModel.transform(input: input)
+    
+    /// 중복체크 결과 > 다음으로 버튼 활성화 여부
+    output.duplicateCheckResult
+      .drive(nextButton.rx.isEnabled)
+      .disposed(by: disposeBag)
+    
+    /// 중복체크 결과 > 다음 버튼 타이틀 업데이트
+    output.duplicateCheckResult
+      .map { $0 ? Constant.Button.next : Constant.Button.duplicateCheckInfo }
+      .drive(nextButton.rx.title())
+      .disposed(by: disposeBag)
+    
+    /// 중복체크 결과 라벨 업데이트
+    output.duplicateCheckResult
+      .do(onNext: { _ in
+        self.duplicateCheckResultInfoLabel.isHidden = false
+      })
+      .debug()
+      .map { $0 ? Constant.Label.avaliableEmailInfo : Constant.Label.duplicatedEmailInfo }
+      .drive(duplicateCheckResultInfoLabel.rx.text)
+      .disposed(by: disposeBag)
+    
+//    output.duplicateCheckResult
+//      .map { _ in false }
+//      .drive(duplicateCheckResultInfoLabel.rx.isHidden)
+//      .disposed(by: disposeBag)
+    
+    /// 이메일이 수정되면 중복체크 결과 라벨 숨기기
+    output.emailChanged
+      .map { true }
+      .drive(duplicateCheckResultInfoLabel.rx.isHidden)
+      .disposed(by: disposeBag)
+    
+    /// 이메일 필드 유효성 검사 > 중복 확인 버튼 활성화 여부
+    emailField.inputValidation
+      .bind(to: duplicateCheckButton.rx.isEnabled)
+      .disposed(by: disposeBag)
+    
+    /// 중복확인 버튼 탭 이벤트 전달
+    duplicateCheckButton.rx.tap
+      .buttonThrottle()
+      .bind(to: input.duplicateCheckEvent)
+      .disposed(by: disposeBag)
+    
+    /// 이메일 필드 값 전달
+    emailField.rx.text.orEmpty
+      .bind(to: input.email)
+      .disposed(by: disposeBag)
   }
 }
 
 @available(iOS 17, *)
 #Preview {
-  SignUpEmailViewController(viewModel: SignUpViewModel())
+  UINavigationController(rootViewController: SignUpEmailViewController(viewModel: SignUpViewModel()))
 }
--- KeyCat/Presentation/Main/Auth/SignUp/SignUpViewModel.swift
+++ KeyCat/Presentation/Main/Auth/SignUp/SignUpViewModel.swift
@@ -12,11 +12,13 @@ final class SignUpViewModel: ViewModel {
   
   // MARK: - I / O
   struct Input {
-    
+    let email: PublishRelay<String>
+    let duplicateCheckEvent: PublishRelay<Void>
   }
   
   struct Output {
-    
+    let emailChanged: Driver<Void>
+    let duplicateCheckResult: Driver<Bool>
   }
   
   // MARK: - Property
@@ -28,9 +30,48 @@ final class SignUpViewModel: ViewModel {
     
   }
   
+  private let checkEmailValidationUsecase: CheckEmailDuplicationUsecase = CheckEmailDuplicationUsecaseImpl()
+  
   // MARK: - Method
   func transform(input: Input) -> Output {
-    return Output()
+    
+    let emailChanged = BehaviorRelay(value: ())
+    let duplicateCheckResult = BehaviorRelay<Bool>(value: false)
+    
+    /// 이메일이 변경되면 중복체크 결과 초기화
+    emailChanged
+      .map { false }
+      .bind(to: duplicateCheckResult)
+      .disposed(by: disposeBag)
+    
+    /// 이메일 변경 이벤트 전달
+    input.email
+      .map { _ in () }
+      .bind(to: emailChanged)
+      .disposed(by: disposeBag)
+    
+    /// 서버에 이메일 중복 검사 요청 후 응답 결과 전달
+    input.duplicateCheckEvent
+      .withLatestFrom(input.email)
+      .withUnretained(self)
+      .flatMap { owner, email in
+        return owner.checkEmailValidationUsecase.execute(email: email)
+          .catch { error in
+            owner.coordinator?.showErrorAlert(error: error)
+            return Single.just(false)
+          }
+      }
+      .bind(to: duplicateCheckResult)
+      .disposed(by: disposeBag)
+
+    
+    return Output(
+      emailChanged: emailChanged.asDriver(),
+      duplicateCheckResult: duplicateCheckResult.asDriver()
+    )
   }
 }
 
+extension Reactive where Base == any ViewModel {
+  
+}
