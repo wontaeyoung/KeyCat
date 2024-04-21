6d01cbd5edd6c83cf4a2835702a60295b2f64879
Taeyoung Won <hexter1994@gmail.com>
2024-04-22 02:47:07 +0900

    #7 fix: 글로벌 토스트 스타일 설정

--- KeyCat/App/AppDelegate.swift
+++ KeyCat/App/AppDelegate.swift
@@ -6,11 +6,15 @@
 //
 
 import UIKit
+import Toast
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+    setGlobalToastStyle()
+    
     return true
   }
 
@@ -22,5 +26,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     
+  }
+}
+
+extension AppDelegate {
+  
+  private func setGlobalToastStyle() {
+    ToastManager.shared.style = ToastStyle().applied {
+      $0.backgroundColor = KCAsset.Color.lightGrayBackground
+      $0.messageColor = KCAsset.Color.black
+      $0.messageFont = KCAsset.Font.toastMessage
+      $0.titleColor = KCAsset.Color.black
+      $0.titleFont = KCAsset.Font.toastTitle
+      $0.titleAlignment = .center
+      $0.activityBackgroundColor = .clear
+      $0.activityIndicatorColor = KCAsset.Color.brand
+    }
   }
 }
