02bcb8f955f38fcf904dc3160a94bd0e59b882bf
Taeyoung Won <hexter1994@gmail.com>
2024-04-22 02:51:30 +0900

    #7 fix: 미사용 라이브러리 import 제거

--- KeyCat/Presentation/Main/Auth/SignUp/SignUpEmailViewController.swift
+++ KeyCat/Presentation/Main/Auth/SignUp/SignUpEmailViewController.swift
@@ -9,8 +9,6 @@ import UIKit
 import SnapKit
 import RxSwift
 import RxCocoa
-import TextFieldEffects
-import Toast
 
 final class SignUpEmailViewController: SignUpBaseViewController, ViewModelController {
   
