1f7d7654abbef28081aa6cfffed9b1203926f828
Taeyoung Won <hexter1994@gmail.com>
2024-04-22 23:56:26 +0900

    #7 fix: 비밀번호 입력 뷰에서 사용하는 옵저버블 이벤트 바인딩

--- KeyCat/Presentation/Main/Auth/SignUp/SignUpViewModel.swift
+++ KeyCat/Presentation/Main/Auth/SignUp/SignUpViewModel.swift
@@ -14,16 +14,28 @@ final class SignUpViewModel: ViewModel {
   struct Input {
     let email: PublishRelay<String>
     let duplicateCheckEvent: PublishRelay<Void>
+    let emailNextEvent: PublishRelay<Void>
+    
+    let password: PublishRelay<String>
+    let passwordCheck: PublishRelay<String>
+    let passwordNextEvent: PublishRelay<Void>
   }
   
   struct Output {
     let duplicateCheckResult: Driver<Bool>
     let showDuplicationCheckResultToast: Driver<Void>
+    
+    let passwordEqualValidationResult: Driver<Bool>
   }
   
   // MARK: - Property
   let disposeBag = DisposeBag()
   weak var coordinator: AuthCoordinator?
+  
+  private let email = BehaviorRelay<String>(value: "")
+  private let password = BehaviorRelay<String>(value: "")
+  private let nickname = BehaviorRelay<String>(value: "")
+  private let sellingAuthority = BehaviorRelay<Bool>(value: false)
   
   // MARK: - Initializer
   init() {
@@ -35,20 +47,24 @@ final class SignUpViewModel: ViewModel {
   // MARK: - Method
   func transform(input: Input) -> Output {
     
-    let emailChanged = BehaviorRelay(value: ())
     let duplicateCheckResult = BehaviorRelay<Bool>(value: false)
     let showDuplicationCheckResultToast = PublishRelay<Void>()
+    let passwrodEqualValidationResult = BehaviorRelay<Bool>(value: false)
     
-    /// 이메일이 변경되면 중복체크 결과 초기화
-    emailChanged
-      .map { false }
-      .bind(to: duplicateCheckResult)
+    /// 이메일 입력내용 전달
+    input.email
+      .bind(to: email)
       .disposed(by: disposeBag)
     
-    /// 이메일 변경 이벤트 전달
+    /// 비밀번호 입력내용 전달
+    input.password
+      .bind(to: password)
+      .disposed(by: disposeBag)
+    
+    /// 이메일이 변경되면 중복체크 결과 초기화
     input.email
-      .map { _ in () }
-      .bind(to: emailChanged)
+      .map { _ in false }
+      .bind(to: duplicateCheckResult)
       .disposed(by: disposeBag)
     
     /// 서버에 이메일 중복 검사 요청 후 응답 결과 전달
@@ -67,10 +83,26 @@ final class SignUpViewModel: ViewModel {
       }
       .disposed(by: disposeBag)
 
+    /// 이메일 다음 버튼 이벤트 화면 연결
+    input.emailNextEvent
+      .bind(with: self) { owner, _ in
+        owner.coordinator?.showSignUpPasswordView()
+      }
+      .disposed(by: disposeBag)
+    
+    /// 비밀번호 == 비밀번호 필드, 두 필드가 모두 비어있지 않은지 검사해서 전달
+    Observable.combineLatest(
+      input.password,
+      input.passwordCheck
+    )
+    .map { $0 == $1 && !$0.isEmpty && !$1.isEmpty }
+    .bind(to: passwrodEqualValidationResult)
+    .disposed(by: disposeBag)
     
     return Output(
       duplicateCheckResult: duplicateCheckResult.asDriver(),
-      showDuplicationCheckResultToast: showDuplicationCheckResultToast.asDriver(onErrorJustReturn: ())
+      showDuplicationCheckResultToast: showDuplicationCheckResultToast.asDriver(onErrorJustReturn: ()),
+      passwordEqualValidationResult: passwrodEqualValidationResult.asDriver()
     )
   }
 }
