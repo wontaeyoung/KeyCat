1fa073fa5c8e6f6f80af438ed93e03440c94fcd4
Taeyoung Won <hexter1994@gmail.com>
2024-04-23 22:39:31 +0900

    #7 add: 사업자 정보 모델 DTO 추가

---
+++ KeyCat/Data/DTO/BusinessInfoDTO.swift
@@ -0,0 +1,12 @@
+//
+//  BusinessInfoDTO.swift
+//  KeyCat
+//
+//  Created by 원태영 on 4/23/24.
+//
+
+struct BusinessInfoDTO: DTO {
+  
+  let b_no: String // 사업자 번호
+  let b_stt_cd: String // 사업자 상태
+}
