b4384db95f1eccfe95295903990a2d362505f64b
Taeyoung Won <hexter1994@gmail.com>
2024-04-24 18:02:24 +0900

    #7 fix: 상태 메세지 추가, 미등록 모델 인스턴스 프로퍼티 추가

--- KeyCat/Domain/Entity/User/BusinessInfo.swift
+++ KeyCat/Domain/Entity/User/BusinessInfo.swift
@@ -11,6 +11,7 @@ struct BusinessInfo: Entity {
   let businessStatus: BusinessStatus
   
   enum BusinessStatus: String {
+    
     case active = "01"
     case suspended = "02"
     case closed = "03"
@@ -19,5 +20,21 @@ struct BusinessInfo: Entity {
     init(_ rawValue: String) {
       self = BusinessStatus(rawValue: rawValue) ?? .unregistered
     }
+    
+    var statusMessage: String {
+      switch self {
+        case .active: "사업자 등록이 확인되었어요."
+        case .suspended: "휴업 상태의 사업자는 판매자 등록이 불가능해요."
+        case .closed: "폐업 상태의 사업자는 판매자 등록이 불가능해요."
+        case .unregistered: "등록 정보를 확인할 수 없어요."
+      }
+    }
+  }
+  
+  static var unregistered: BusinessInfo {
+    return BusinessInfo(
+      businessNumber: "-",
+      businessStatus: .unregistered
+    )
   }
 }
