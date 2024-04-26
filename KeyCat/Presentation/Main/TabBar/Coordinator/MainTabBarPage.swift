2c0d4f475f42c0789eff1a8f16bfe6054e4c5c75
Taeyoung Won <hexter1994@gmail.com>
2024-04-27 00:16:46 +0900

    #9 add: 탭바 페이지 관리 열거형 추가

---
+++ KeyCat/Presentation/Main/TabBar/Coordinator/MainTabBarPage.swift
@@ -0,0 +1,66 @@
+//
+//  MainTabBarPage.swift
+//  KeyCat
+//
+//  Created by 원태영 on 4/26/24.
+//
+
+import UIKit
+
+enum MainTabBarPage: Int, CaseIterable {
+  
+  case home
+  case shopping
+  case profile
+  
+  var index: Int {
+    self.rawValue
+  }
+  
+  var title: String {
+    switch self {
+      case .home:
+        return "홈"
+        
+      case .shopping:
+        return "쇼핑"
+        
+      case .profile:
+        return "마이페이지"
+    }
+  }
+  
+  var icon: UIImage? {
+    switch self {
+      case .home:
+        return KCAsset.Symbol.homeTab
+        
+      case .shopping:
+        return KCAsset.Symbol.shoppingTab
+        
+      case .profile:
+        return KCAsset.Symbol.profileTab
+    }
+  }
+  
+  var selectedIcon: UIImage? {
+    switch self {
+      case .home:
+        return KCAsset.Symbol.homeSelectedTab
+        
+      case .shopping:
+        return KCAsset.Symbol.shoppingSelectedTab
+        
+      case .profile:
+        return KCAsset.Symbol.profileSelectedTab
+    }
+  }
+  
+  var tabBarItem: UITabBarItem {
+    return UITabBarItem(
+      title: title,
+      image: icon,
+      selectedImage: selectedIcon
+    )
+  }
+}
