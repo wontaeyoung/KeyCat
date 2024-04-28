361ecaa697c48dde855df4a606a75119419227bc
Taeyoung Won <hexter1994@gmail.com>
2024-04-28 21:37:15 +0900

    #9 add: 판매글 작성 뷰모델 클래스 추가

---
+++ KeyCat/Presentation/Main/TabBar/Shopping/CreateCommercialPost/CreateCommercialPostViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  CreateCommercialPostViewModel.swift
+//  KeyCat
+//
+//  Created by 원태영 on 4/28/24.
+//
+
+import RxSwift
+import RxCocoa
+
+final class CreateCommercialPostViewModel: ViewModel {
+  
+  // MARK: - I / O
+  struct Input {
+    
+  }
+  
+  struct Output {
+    
+  }
+  
+  // MARK: - Property
+  let disposeBag = DisposeBag()
+  weak var coordinator: ShoppingCoordinator?
+  
+  // MARK: - Initializer
+  init() {
+    
+  }
+  
+  // MARK: - Method
+  func transform(input: Input) -> Output {
+    
+    return Output()
+  }
+}
