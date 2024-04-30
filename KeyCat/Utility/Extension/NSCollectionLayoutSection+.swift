55a39e60878ac90040200fc474d423527a3430ce
Taeyoung Won <hexter1994@gmail.com>
2024-04-30 15:56:17 +0900

    #9 fix: 가로 스크롤 그리드 레이아웃 생성 메서드 구현

--- KeyCat/Utility/Extension/NSCollectionLayoutSection+.swift
+++ KeyCat/Utility/Extension/NSCollectionLayoutSection+.swift
@@ -18,15 +18,17 @@ extension NSCollectionLayoutSection {
     
     let item: NSCollectionLayoutItem = NSCollectionLayoutItem(
       layoutSize: NSCollectionLayoutSize(
-        widthDimension: .fractionalWidth(0.3),
+//        widthDimension: .fractionalWidth(0.3),
+//        heightDimension: .fractionalHeight(1)
+        widthDimension: .fractionalWidth(1),
         heightDimension: .fractionalHeight(1)
       )
     )
     
     let group: NSCollectionLayoutGroup = .horizontal(
       layoutSize: NSCollectionLayoutSize(
-        widthDimension: .fractionalWidth(1),
-        heightDimension: .fractionalWidth(0.3)
+        widthDimension: .absolute(100),
+        heightDimension: .absolute(100)
       ),
       subitems: [item]
     )
@@ -36,12 +38,16 @@ extension NSCollectionLayoutSection {
     let section = NSCollectionLayoutSection(group: group).configured {
       $0.orthogonalScrollingBehavior = .continuousGroupLeadingBoundary
       $0.contentInsets = sectionInset
+      $0.interGroupSpacing = itemSpacing
     }
     
     if header {
       section.boundarySupplementaryItems = [
         NSCollectionLayoutBoundarySupplementaryItem(
-          layoutSize: .init(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(headerHeight)),
+          layoutSize: .init(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(headerHeight)
+          ),
           elementKind: UICollectionView.elementKindSectionHeader,
           alignment: .top
         )
