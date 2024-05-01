a0da62d789052aac9d280146b77f2871e197f77e
Taeyoung Won <hexter1994@gmail.com>
2024-05-02 01:23:55 +0900

    #9 add: 평균 리뷰 점수 계산 익스텐션 구현

---
+++ KeyCat/Utility/Extension/Array+.swift
@@ -0,0 +1,15 @@
+//
+//  Array+.swift
+//  KeyCat
+//
+//  Created by 원태영 on 5/1/24.
+//
+
+extension Array where Element == CommercialReview {
+  var averageScore: Double {
+    let totalScore = map { $0.rating.rawValue }
+      .reduce(0, +)
+    
+    return Double(totalScore) / Double(count)
+  }
+}
