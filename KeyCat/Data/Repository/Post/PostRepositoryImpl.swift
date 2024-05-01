5f907dfc5d31d828eabef55cc8baeac6f8de1d7f
Taeyoung Won <hexter1994@gmail.com>
2024-05-01 18:55:53 +0900

    #9 add: PostRepository 추가, 이미지 업로드 / 게시물 작성 메서드 구현

---
+++ KeyCat/Data/Repository/Post/PostRepositoryImpl.swift
@@ -0,0 +1,49 @@
+//
+//  PostRepositoryImpl.swift
+//  KeyCat
+//
+//  Created by 원태영 on 5/1/24.
+//
+
+import Foundation
+import RxSwift
+
+final class PostRepositoryImpl: PostRepository, HTTPErrorTransformer {
+  
+  private let service: APIService
+  private let postMapper: PostMapper
+  
+  init(
+    service: APIService = APIService(),
+    postMapper: PostMapper = PostMapper()
+  ) {
+    self.service = service
+    self.postMapper = postMapper
+  }
+  
+  func uploadPostImages(files: [Data]) -> Single<[CommercialPost.URLString]> {
+    let router = PostRouter.postImageUpload
+    
+    return service.callImageUploadRequest(data: files)
+      .catch {
+        let domainError = self.httpErrorToDomain(from: $0, style: .accessToken)
+        return .error(domainError)
+      }
+      .map { $0.files }
+  }
+  
+  func createCommercialPost(post: CommercialPost, isUpdateImages: Bool) -> Single<CommercialPost?> {
+    guard let request = postMapper.toRequest(post, isUpdateImages: isUpdateImages) else {
+      return .error(KCError.missingRequired)
+    }
+    
+    let router = PostRouter.postCreate(request: request)
+    
+    return service.callRequest(with: router, of: PostDTO.self)
+      .catch {
+        let domainError = self.httpErrorToDomain(from: $0, style: .createPost)
+        return .error(domainError)
+      }
+      .map { self.postMapper.toEntity($0) }
+  }
+}
---
+++ KeyCat/Domain/Repository/Post/PostRepository.swift
@@ -0,0 +1,15 @@
+//
+//  PostRepository.swift
+//  KeyCat
+//
+//  Created by 원태영 on 5/1/24.
+//
+
+import Foundation
+import RxSwift
+
+protocol PostRepository {
+  
+  func uploadPostImages(files: [Data]) -> Single<[CommercialPost.URLString]>
+  func createCommercialPost(post: CommercialPost, isUpdateImages: Bool) -> Single<CommercialPost?>
+}
