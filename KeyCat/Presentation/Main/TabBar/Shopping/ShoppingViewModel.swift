3a5314e291394841d7d20f98a18702c7368887b8
Taeyoung Won <hexter1994@gmail.com>
2024-05-02 01:27:20 +0900

    #9 fix: 더미 포스트 데이터 추가, 커머스 상품 리스트 output 추가

--- KeyCat/Presentation/Main/TabBar/Shopping/ShoppingViewModel.swift
+++ KeyCat/Presentation/Main/TabBar/Shopping/ShoppingViewModel.swift
@@ -5,6 +5,7 @@
 //  Created by 원태영 on 4/27/24.
 //
 
+import Foundation
 import RxSwift
 import RxCocoa
 
@@ -26,6 +27,7 @@ final class ShoppingViewModel: ViewModel {
   
   struct Output {
     let hasSellerAuthority: Driver<Bool>
+    let commercialPosts: Driver<[CommercialPost]>
   }
   
   // MARK: - Property
@@ -41,10 +43,16 @@ final class ShoppingViewModel: ViewModel {
   func transform(input: Input) -> Output {
     
     let hasSellerAuthority = PublishRelay<Bool>()
+    let commercialPosts = PublishRelay<[CommercialPost]>()
     
     input.viewDidLoadEvent
       .map { UserInfoService.hasSellerAuthority }
       .bind(to: hasSellerAuthority)
+      .disposed(by: disposeBag)
+    
+    input.viewDidLoadEvent
+      .map { self.makeDummyPosts() }
+      .bind(to: commercialPosts)
       .disposed(by: disposeBag)
     
     input.createPostTapEvent
@@ -53,6 +61,80 @@ final class ShoppingViewModel: ViewModel {
       }
       .disposed(by: disposeBag)
     
-    return Output(hasSellerAuthority: hasSellerAuthority.asDriver(onErrorJustReturn: false))
+    return Output(
+      hasSellerAuthority: hasSellerAuthority.asDriver(onErrorJustReturn: false),
+      commercialPosts: commercialPosts.asDriver(onErrorJustReturn: [])
+    )
+  }
+  
+  private func makeDummyPosts() -> [CommercialPost] {
+    print("시작", Date.now)
+    let posts: [CommercialPost] = (1...20).map { n in
+        CommercialPost(
+          postID: n.description,
+          postType: .keycat_commercialProduct,
+          title: "키보드 상품 타이틀 \(n)",
+          content: "상품 설명입니다 \(n)",
+          keyboard: .init(
+            keyboardInfo: .init(
+              purpose: .allCases.randomElement()!,
+              inputMechanism: .allCases.randomElement()!,
+              connectionType: .allCases.randomElement()!,
+              powerSource: .allCases.randomElement()!,
+              backlight: .allCases.randomElement()!,
+              pcbType: .allCases.randomElement()!,
+              mechanicalSwitch: .allCases.randomElement()!,
+              capacitiveSwitch: .allCases.randomElement()!
+            ),
+            keycapInfo: .init(
+              profile: .allCases.randomElement()!,
+              direction: .allCases.randomElement()!,
+              process: .allCases.randomElement()!,
+              language: .allCases.randomElement()!
+            ),
+            keyboardAppearanceInfo: .init(
+              ratio: .allCases.randomElement()!,
+              design: .allCases.randomElement()!,
+              material: .allCases.randomElement()!,
+              size: .init(width: 380, height: 150, depth: 40, weight: 1062)
+            )
+          ),
+          price: .init(
+            regularPrice: n * 1000,
+            couponPrice: [0, 1000].randomElement()!,
+            discountPrice: [0, 250, 500].randomElement()! * n,
+            discountExpiryDate: DateManager.shared.date(from: .now, as: .day, by: n)
+          ),
+          delivery: .init(
+            price: .allCases.randomElement()!,
+            schedule: .allCases.randomElement()!
+          ),
+          createdAt: DateManager.shared.date(from: .now, as: .hour, by: n),
+          creator: .init(
+            userID: n.description,
+            nickname: "닉네임 \(n)",
+            profileImageURLString: ""
+          ),
+          files: ["uploads/posts/vamillo_1_1714571161018.jpg"],
+          likes: [["662a499ea8bf9f5c9ca667a8"], []].randomElement()!,
+          shoppingCarts: [["662a499ea8bf9f5c9ca667a8"], []].randomElement()!,
+          hashTags: [],
+          reviews: [
+            .init(
+              reviewID: n.description,
+              content: "리뷰 \(n)",
+              rating: .allCases.randomElement()!,
+              createdAt: DateManager.shared.date(from: .now, as: .hour, by: n),
+              creator: .init(
+                userID: n.description,
+                nickname: "닉네임 \(n+1)",
+                profileImageURLString: ""
+              )
+            )
+          ]
+        )
+    }
+    print("종료", Date.now)
+    return posts
   }
 }
