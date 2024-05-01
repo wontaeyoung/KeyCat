f395dd6e5a93880339681806aaec7f2197fd6447
Taeyoung Won <hexter1994@gmail.com>
2024-05-01 13:45:04 +0900

    #9 fix: 네트워크 단절 상태 표시 뷰 업데이트 로직 구현

--- KeyCat/Presentation/Common/Base/RxBaseViewController.swift
+++ KeyCat/Presentation/Common/Base/RxBaseViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 import RxSwift
 import RxCocoa
 
@@ -13,6 +14,34 @@ class RxBaseViewController: UIViewController {
   
   class var identifier: String {
     return self.description()
+  }
+  
+  private let networkUnsatisfiedView = UIView().configured {
+    let imageView = UIImageView(image: KCAsset.Symbol.networkDisconnect).configured {
+      $0.tintColor = KCAsset.Color.darkGray
+    }
+    
+    let label = KCLabel(
+      style: .standardTitle,
+      title: "네트워크가 원활하지 않습니다.\n연결 상태를 다시 확인해주세요.",
+      alignment: .center
+    )
+      .configured {
+        $0.numberOfLines = 2
+      }
+    
+    $0.addSubviews(imageView, label)
+    
+    imageView.snp.makeConstraints { make in
+      make.centerX.equalToSuperview()
+      make.bottom.equalTo(label.snp.top).offset(-20)
+      make.size.equalTo(100)
+    }
+    
+    label.snp.makeConstraints { make in
+      make.centerY.equalToSuperview()
+      make.horizontalEdges.equalToSuperview().inset(20)
+    }
   }
   
   // MARK: - Property
@@ -30,6 +59,15 @@ class RxBaseViewController: UIViewController {
       .buttonThrottle(seconds: 1)
       .map { _ in () }
       .bind(to: tap)
+      .disposed(by: disposeBag)
+    
+    /// Window 갈아끼우기 구현
+    NetworkMonitor.shared.networkStateSatisfied
+      .thread(.main)
+      .map { _ in false }
+      .bind(with: self) { owner, isSatisfied in
+        owner.updateNetworkUnsatisfiedViewVisibility(isSatisfied: isSatisfied)
+      }
       .disposed(by: disposeBag)
   }
   
@@ -54,5 +92,19 @@ class RxBaseViewController: UIViewController {
     setConstraint()
     setAttribute()
     bind()
+  }
+  
+  private func updateNetworkUnsatisfiedViewVisibility(isSatisfied: Bool) {
+    
+    if isSatisfied {
+      networkUnsatisfiedView.snp.removeConstraints()
+      networkUnsatisfiedView.removeFromSuperview()
+    } else {
+      view.addSubview(networkUnsatisfiedView)
+      
+      networkUnsatisfiedView.snp.makeConstraints { make in
+        make.edges.equalTo(view)
+      }
+    }
   }
 }
