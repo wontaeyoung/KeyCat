b22676269d6031405fcc337dbe453a973ebac245
Taeyoung Won <hexter1994@gmail.com>
2024-05-02 02:01:24 +0900

    #9 fix: 컬렉션 셀 높이 증가

--- KeyCat/Utility/Extension/UICollectionViewFlowLayout+.swift
+++ KeyCat/Utility/Extension/UICollectionViewFlowLayout+.swift
@@ -13,7 +13,7 @@ extension UICollectionViewFlowLayout {
     let cellWidth: CGFloat = (UIScreen.main.bounds.width - (cellSpacing * (2 + cellCount - 1))) / cellCount
     
     return UICollectionViewFlowLayout().configured {
-      $0.itemSize = CGSize(width: cellWidth, height: cellWidth * 2)
+      $0.itemSize = CGSize(width: cellWidth, height: cellWidth * 2.5)
       $0.sectionInset = UIEdgeInsets(top: cellSpacing, left: cellSpacing, bottom: cellSpacing, right: cellSpacing)
       $0.minimumInteritemSpacing = cellSpacing
       $0.scrollDirection = .vertical
