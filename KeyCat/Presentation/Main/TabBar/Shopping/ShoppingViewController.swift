ccb9baf612463121bba4828e8a539e1998ef2a49
Taeyoung Won <hexter1994@gmail.com>
2024-05-02 01:27:41 +0900

    #9 fix: 상품 컬렉션 뷰 구현, 데이터 바인딩

--- KeyCat/Presentation/Main/TabBar/Shopping/ShoppingViewController.swift
+++ KeyCat/Presentation/Main/TabBar/Shopping/ShoppingViewController.swift
@@ -25,6 +25,16 @@ final class ShoppingViewController: RxBaseViewController, ViewModelController {
     }
   }
   
+  private let productCollectionView = UICollectionView(
+    frame: .zero,
+    collectionViewLayout: UICollectionViewFlowLayout.gridLayout(cellCount: 2, cellSpacing: 20)
+  ).configured {
+    $0.register(
+      ProductCollectionCell.self,
+      forCellWithReuseIdentifier: ProductCollectionCell.identifier
+    )
+  }
+  
   // MARK: - Property
   let viewModel: ShoppingViewModel
   
@@ -37,7 +47,10 @@ final class ShoppingViewController: RxBaseViewController, ViewModelController {
   
   // MARK: - Life Cycle
   override func setHierarchy() {
-    view.addSubviews(createPostFloatingButton)
+    view.addSubviews(
+      productCollectionView,
+      createPostFloatingButton
+    )
   }
   
   override func setConstraint() {
@@ -45,6 +58,10 @@ final class ShoppingViewController: RxBaseViewController, ViewModelController {
       make.trailing.equalTo(view).inset(20)
       make.bottom.equalTo(view.safeAreaLayoutGuide).inset(20)
       make.size.equalTo(50)
+    }
+    
+    productCollectionView.snp.makeConstraints { make in
+      make.edges.equalTo(view.safeAreaLayoutGuide)
     }
   }
   
@@ -60,6 +77,15 @@ final class ShoppingViewController: RxBaseViewController, ViewModelController {
     output.hasSellerAuthority
       .map { !$0 }
       .drive(createPostFloatingButton.rx.isHidden)
+      .disposed(by: disposeBag)
+    
+    output.commercialPosts
+      .drive(productCollectionView.rx.items(
+        cellIdentifier: ProductCollectionCell.identifier,
+        cellType: ProductCollectionCell.self)
+      ) { row, item, cell in
+        cell.setData(post: item)
+      }
       .disposed(by: disposeBag)
     
     createPostFloatingButton.rx.tap
