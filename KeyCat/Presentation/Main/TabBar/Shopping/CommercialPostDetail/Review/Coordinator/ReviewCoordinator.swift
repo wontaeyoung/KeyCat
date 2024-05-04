775b90767c16159217cf116b102308b21c107028
Taeyoung Won <hexter1994@gmail.com>
2024-05-05 00:46:02 +0900

    #13 fix: 리뷰 작성 화면 연결 함수 구현

--- KeyCat/Presentation/Main/TabBar/Shopping/CommercialPostDetail/Review/Coordinator/ReviewCoordinator.swift
+++ KeyCat/Presentation/Main/TabBar/Shopping/CommercialPostDetail/Review/Coordinator/ReviewCoordinator.swift
@@ -19,20 +19,32 @@ final class ReviewCoordinator: SubCoordinator {
   }
   
   func start() {
-    showReviewListView()
+    
   }
 }
 
 extension ReviewCoordinator {
   
-  func showReviewListView() {
+  func showReviewListView(post: CommercialPost) {
     
-    let vm = ReviewListViewModel()
+    let vm = ReviewListViewModel(post: post)
       .coordinator(self)
     
     let vc = ReviewListViewController(viewModel: vm)
       .hideBackTitle()
       .navigationTitle(with: "리뷰 모아보기", displayMode: .never)
+    
+    push(vc)
+  }
+  
+  func showCreateCommercialReviewView(post: CommercialPost) {
+    
+    let vm = CreateCommercialReviewViewModel(post: post)
+      .coordinator(self)
+    
+    let vc = CreateCommercialReviewViewController(viewModel: vm)
+      .hideBackTitle()
+      .navigationTitle(with: "리뷰 쓰기", displayMode: .never)
     
     push(vc)
   }
