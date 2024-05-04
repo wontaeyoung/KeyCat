9b4aab4b63fa1b8ed50917d172ddc3654761e3d8
Taeyoung Won <hexter1994@gmail.com>
2024-05-05 02:14:25 +0900

    #13 add: 리뷰 레포지토리 추가, 리뷰 추가 메서드 구현

---
+++ KeyCat/Domain/Repository/Review/ReviewRepository.swift
@@ -0,0 +1,13 @@
+//
+//  ReviewRepository.swift
+//  KeyCat
+//
+//  Created by 원태영 on 5/5/24.
+//
+
+import RxSwift
+
+protocol ReviewRepository {
+  
+  func createCommercialReview(postID: CommercialPost.PostID, review: CommercialReview) -> Single<CommercialReview?>
+}
