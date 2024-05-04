257d6b399e1a3d8e0d6abb71892d96d0b81877ae
Taeyoung Won <hexter1994@gmail.com>
2024-05-05 03:16:53 +0900

    #13 fix: 리뷰 상세화면 UI 구현, 데이터 바인딩

--- KeyCat/Presentation/Main/TabBar/Shopping/CommercialPostDetail/Review/CommercialReviewDetail/CommercialReviewDetailViewController.swift
+++ KeyCat/Presentation/Main/TabBar/Shopping/CommercialPostDetail/Review/CommercialReviewDetail/CommercialReviewDetailViewController.swift
@@ -13,10 +13,30 @@ import RxCocoa
 final class CommercialReviewDetailViewController: RxBaseViewController, ViewModelController {
   
   // MARK: - UI
+  private let scrollView = UIScrollView()
+  private let contentView = UIView()
+  private let containerView = UIView()
   
+  private lazy var profileImageView = TappableImageView(image: nil).configured {
+    $0.clipsToBounds = true
+    $0.layer.configure {
+      $0.cornerRadius = profileImageSize / 2
+      $0.borderWidth = 1
+      $0.borderColor = KCAsset.Color.lightGrayForeground.color.cgColor
+    }
+  }
+  private let nicknameLabel = KCLabel(font: .medium(size: 13))
+  private let dateLabel = KCLabel(font: .medium(size: 13), color: .darkGray, alignment: .right)
+  private lazy var reviewStarStack = UIStackView().configured {
+    $0.axis = .horizontal
+    $0.spacing = 2
+  }
+  private let contentLabel = KCLabel(font: .medium(size: 13), line: 0, isUpdatingLineSpacing: true)
   
   // MARK: - Property
   let viewModel: CommercialReviewDetailViewModel
+  
+  private let profileImageSize: CGFloat = 40
   
   // MARK: - Initializer
   init(viewModel: CommercialReviewDetailViewModel) {
@@ -27,18 +47,97 @@ final class CommercialReviewDetailViewController: RxBaseViewController, ViewMode
   
   // MARK: - Life Cycle
   override func setHierarchy() {
+    view.addSubviews(scrollView)
+    scrollView.addSubviews(contentView)
+    contentView.addSubviews(containerView)
     
+    containerView.addSubviews(
+      profileImageView,
+      nicknameLabel,
+      dateLabel,
+      reviewStarStack,
+      contentLabel
+    )
   }
   
   override func setConstraint() {
+    scrollView.snp.makeConstraints { make in
+      make.edges.equalTo(view.safeAreaLayoutGuide)
+    }
     
-  }
-  
-  override func setAttribute() {
+    contentView.snp.makeConstraints { make in
+      make.width.equalTo(scrollView)
+      make.verticalEdges.equalTo(scrollView)
+    }
     
+    containerView.snp.makeConstraints { make in
+      make.edges.equalToSuperview().inset(20)
+    }
+    
+    profileImageView.snp.makeConstraints { make in
+      make.top.equalToSuperview()
+      make.leading.equalToSuperview()
+      make.size.equalTo(profileImageSize)
+    }
+    
+    nicknameLabel.snp.makeConstraints { make in
+      make.leading.equalTo(profileImageView.snp.trailing).offset(10)
+      make.centerY.equalTo(profileImageView)
+    }
+    
+    dateLabel.snp.makeConstraints { make in
+      make.leading.equalTo(nicknameLabel.snp.trailing).offset(10)
+      make.centerY.equalTo(profileImageView)
+      make.trailing.equalToSuperview()
+    }
+    
+    reviewStarStack.snp.makeConstraints { make in
+      make.top.equalTo(profileImageView.snp.bottom).offset(5)
+      make.leading.equalToSuperview()
+    }
+    
+    contentLabel.snp.makeConstraints { make in
+      make.top.equalTo(reviewStarStack.snp.bottom).offset(20)
+      make.horizontalEdges.equalToSuperview()
+      make.bottom.lessThanOrEqualToSuperview()
+    }
   }
   
   override func bind() {
     
+    let input = CommercialReviewDetailViewModel.Input()
+    let output = viewModel.transform(input: input)
+    
+    output.review
+      .drive(with: self) { owner, review in
+        owner.setData(review: review)
+      }
+      .disposed(by: disposeBag)
+    
+    profileImageView.tap
+      .buttonThrottle()
+      .bind(to: input.profileTapEvent)
+      .disposed(by: disposeBag)
+  }
+  
+  private func setData(review: CommercialReview) {
+    profileImageView.load(with: review.creator.profileImageURL)
+    nicknameLabel.text = review.creator.nickname
+    dateLabel.text = review.dateString
+    contentLabel.text = review.content
+    
+    stackingReviewStar(rating: review.rating)
+  }
+  
+  private func stackingReviewStar(rating: CommercialReview.Rating) {
+    (1...5).forEach { num in
+      let imageView = UIImageView().configured {
+        $0.contentMode = .scaleAspectFit
+        $0.image = rating.rawValue >= num ? KCAsset.Symbol.reviewScore : KCAsset.Symbol.emptyReviewScore
+        $0.snp.makeConstraints { make in make.size.equalTo(30) }
+      }
+      
+      reviewStarStack.addArrangedSubview(imageView)
+    }
   }
 }
--- KeyCat/Presentation/Main/TabBar/Shopping/CommercialPostDetail/Review/CommercialReviewDetail/CommercialReviewDetailViewModel.swift
+++ KeyCat/Presentation/Main/TabBar/Shopping/CommercialPostDetail/Review/CommercialReviewDetail/CommercialReviewDetailViewModel.swift
@@ -12,25 +12,43 @@ final class CommercialReviewDetailViewModel: ViewModel {
   
   // MARK: - I / O
   struct Input {
+    let profileTapEvent: PublishRelay<Void>
     
+    init(
+      profileTapEvent: PublishRelay<Void> = .init()
+    ) {
+      self.profileTapEvent = profileTapEvent
+    }
   }
   
   struct Output {
-    
+    let review: Driver<CommercialReview>
   }
   
   // MARK: - Property
   let disposeBag = DisposeBag()
   weak var coordinator: ReviewCoordinator?
   
+  private let postID: CommercialPost.PostID
+  private let review: BehaviorRelay<CommercialReview>
+  private let reviews: BehaviorRelay<[CommercialReview]>
+  
   // MARK: - Initializer
-  init() {
-    
+  init(
+    postID: CommercialPost.PostID,
+    review: CommercialReview,
+    reviews: BehaviorRelay<[CommercialReview]>
+  ) {
+    self.postID = postID
+    self.review = BehaviorRelay(value: review)
+    self.reviews = reviews
   }
   
   // MARK: - Method
   func transform(input: Input) -> Output {
     
-    return Output()
+    return Output(
+      review: review.asDriver()
+    )
   }
 }
