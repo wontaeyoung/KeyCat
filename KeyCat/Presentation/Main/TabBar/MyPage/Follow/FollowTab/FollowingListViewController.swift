c476a30dd836afd5163d7fbc85ae2c2582a5d223
Taeyoung Won <hexter1994@gmail.com>
2024-05-05 13:28:10 +0900

    #15 add: 빈 탭맨 팔로우 하위 뷰컨트롤러 추가

---
+++ KeyCat/Presentation/Main/TabBar/MyPage/Follow/FollowTab/FollowingListViewController.swift
@@ -0,0 +1,44 @@
+//
+//  FollowingListViewController.swift
+//  KeyCat
+//
+//  Created by 원태영 on 5/5/24.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+final class FollowingListViewController: RxBaseViewController, ViewModelController {
+  
+  // MARK: - UI
+  
+  
+  // MARK: - Property
+  let viewModel: FollowListViewModel
+  
+  // MARK: - Initializer
+  init(viewModel: FollowListViewModel) {
+    self.viewModel = viewModel
+    
+    super.init()
+  }
+  
+  // MARK: - Life Cycle
+  override func setHierarchy() {
+    
+  }
+  
+  override func setConstraint() {
+    
+  }
+  
+  override func setAttribute() {
+    
+  }
+  
+  override func bind() {
+    
+  }
+}
