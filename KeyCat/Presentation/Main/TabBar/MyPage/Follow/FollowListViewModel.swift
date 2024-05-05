a8af0a06cfdb82ff1c5a8b9a3c0a64656b2e6867
Taeyoung Won <hexter1994@gmail.com>
2024-05-05 13:59:13 +0900

    #15 fix: 프로필 더미데이터 방출

--- KeyCat/Presentation/Main/TabBar/MyPage/Follow/FollowListViewModel.swift
+++ KeyCat/Presentation/Main/TabBar/MyPage/Follow/FollowListViewModel.swift
@@ -16,21 +16,33 @@ final class FollowListViewModel: ViewModel {
   }
   
   struct Output {
-    
+    let profile: Driver<Profile>
   }
   
   // MARK: - Property
   let disposeBag = DisposeBag()
   weak var coordinator: MyPageCoordinator?
+  private let profile: BehaviorRelay<Profile>
   
   // MARK: - Initializer
-  init() {
+  init(profile: BehaviorRelay<Profile>) {
+    let profile = profile
+    var profileValue = profile.value
+    profileValue.folllowing = (1...15).map {
+      User(userID: "id\($0)", nickname: "닉네임 \($0)", profileImageURLString: "")
+    }
     
+    profileValue.followers = (1...10).map {
+      User(userID: "id\($0)", nickname: "이름 \($0)", profileImageURLString: "")
+    }
+    profile.accept(profileValue)
+    
+    self.profile = profile
   }
   
   // MARK: - Method
   func transform(input: Input) -> Output {
    
-    return Output()
+    return Output(profile: profile.asDriver())
   }
 }
