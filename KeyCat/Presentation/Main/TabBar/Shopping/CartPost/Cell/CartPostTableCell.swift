6596bc5835b8a63bff7e811afc30f8e3555b8bd2
Taeyoung Won <hexter1994@gmail.com>
2024-05-07 21:44:43 +0900

    add: 장바구니 게시물 셀 추가

---
+++ KeyCat/Presentation/Main/TabBar/Shopping/CartPost/Cell/CartPostTableCell.swift
@@ -0,0 +1,102 @@
+//
+//  CartPostTableCell.swift
+//  KeyCat
+//
+//  Created by 원태영 on 5/7/24.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+final class CartPostTableCell: RxBaseTableViewCell {
+  
+  // MARK: - UI
+  private let containerView = UIView()
+  private let checkboxButton = CheckboxButton()
+  private let productImageView = UIImageView().configured {
+    $0.contentMode = .scaleAspectFill
+    $0.clipsToBounds = true
+    $0.layer.configure {
+      $0.cornerRadius = 10
+      $0.borderColor = KCAsset.Color.lightGrayBackground.color.cgColor
+      $0.borderWidth = 1
+    }
+  }
+  private let titleLabel = KCLabel(font: .medium(size: 16), line: 2)
+  private let deliveryInfoLabel = KCLabel(font: .medium(size: 13), color: .lightGrayForeground)
+  private let priceView = ProductPriceView()
+  private let deleteButton = KCButton(style: .icon, image: KCAsset.Symbol.leaveButton)
+  
+  // MARK: - Property
+  
+  // MARK: - Life Cycle
+  override func setHierarchy() {
+    
+    contentView.addSubviews(
+      checkboxButton,
+      containerView,
+      deleteButton
+    )
+    
+    containerView.addSubviews(
+      productImageView,
+      titleLabel,
+      deliveryInfoLabel,
+      priceView
+    )
+  }
+  
+  override func setConstraint() {
+    
+    checkboxButton.snp.makeConstraints { make in
+      make.leading.equalToSuperview().inset(10)
+      make.centerY.equalToSuperview()
+      make.size.equalTo(25)
+    }
+    
+    containerView.snp.makeConstraints { make in
+      make.leading.equalTo(checkboxButton.snp.trailing).offset(10)
+      make.verticalEdges.equalToSuperview().inset(20)
+      make.trailing.equalTo(deleteButton.snp.leading).offset(-10)
+    }
+    
+    deleteButton.snp.makeConstraints { make in
+      make.top.equalToSuperview().inset(10)
+      make.trailing.equalToSuperview().inset(10)
+      make.size.equalTo(35)
+    }
+    
+    productImageView.snp.makeConstraints { make in
+      make.top.equalToSuperview()
+      make.leading.equalToSuperview()
+      make.size.equalTo(80)
+    }
+    
+    titleLabel.snp.makeConstraints { make in
+      make.top.equalToSuperview()
+      make.leading.equalTo(productImageView.snp.trailing).offset(20)
+      make.trailing.equalToSuperview()
+    }
+    
+    priceView.snp.makeConstraints { make in
+      make.top.equalTo(titleLabel.snp.bottom).offset(10)
+      make.horizontalEdges.equalTo(titleLabel)
+    }
+    
+    deliveryInfoLabel.snp.makeConstraints { make in
+      make.top.equalTo(priceView.snp.bottom).offset(10)
+      make.horizontalEdges.equalTo(titleLabel)
+      make.bottom.equalToSuperview()
+    }
+  }
+  
+  func setData(post: CommercialPost) {
+    
+    productImageView.load(with: post.mainImageURL)
+    titleLabel.text = post.title
+    priceView.productPrice = post.price
+    deliveryInfoLabel.text = "\(post.delivery.price.name) | \(post.delivery.schedule.name)배송"
+  }
+}
