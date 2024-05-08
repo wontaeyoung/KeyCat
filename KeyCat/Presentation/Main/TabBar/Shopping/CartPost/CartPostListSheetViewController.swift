2237aa6ed831783ce8713d6a59b86b664aad96a4
Taeyoung Won <hexter1994@gmail.com>
2024-05-08 17:34:25 +0900

    add: 장바구니 시트 뷰컨트롤러, 테이블 셀 구현

---
+++ KeyCat/Presentation/Main/TabBar/Shopping/CartPost/CartPostListSheetViewController.swift
@@ -0,0 +1,86 @@
+//
+//  CartPostListSheetViewController.swift
+//  KeyCat
+//
+//  Created by 원태영 on 5/8/24.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+final class CartPostListSheetViewController: RxBaseViewController, ViewModelController {
+  
+  // MARK: - UI
+  private let postAddedInfoLabel = KCLabel(title: "장바구니에 상품을 담았어요.", font: .bold(size: 16))
+  private let shortCutButton = KCButton(style: .secondary, title: "바로가기")
+  private let tableView = UITableView().configured {
+    $0.register(
+      CartPostSheetTableCell.self,
+      forCellReuseIdentifier: CartPostSheetTableCell.identifier
+    )
+  }
+  
+  // MARK: - Property
+  private let cartPosts: BehaviorRelay<[CommercialPost]>
+  let viewModel: CommercialPostDetailViewModel
+  
+  // MARK: - Initializer
+  init(cartPosts: BehaviorRelay<[CommercialPost]>, viewModel: CommercialPostDetailViewModel) {
+    self.cartPosts = cartPosts
+    self.viewModel = viewModel
+    
+    super.init()
+  }
+  
+  // MARK: - Life Cycle
+  override func setHierarchy() {
+    
+    view.addSubviews(
+      postAddedInfoLabel,
+      shortCutButton,
+      tableView
+    )
+  }
+  
+  override func setConstraint() {
+    
+    postAddedInfoLabel.snp.makeConstraints { make in
+      make.leading.equalToSuperview().inset(20)
+      make.centerY.equalTo(shortCutButton)
+    }
+    
+    shortCutButton.snp.makeConstraints { make in
+      make.top.equalTo(view.safeAreaLayoutGuide).inset(20)
+      make.trailing.equalToSuperview().inset(20)
+    }
+    
+    tableView.snp.makeConstraints { make in
+      make.top.equalTo(shortCutButton.snp.bottom).offset(20)
+      make.horizontalEdges.equalToSuperview()
+      make.bottom.equalTo(view.safeAreaLayoutGuide)
+    }
+  }
+  
+  override func bind() {
+    
+    let input = CommercialPostDetailViewModel.Input()
+    let output = viewModel.transform(input: input)
+    
+    shortCutButton.rx.tap
+      .buttonThrottle()
+      .bind(to: input.cartShortCutTapEvent)
+      .disposed(by: disposeBag)
+    
+    cartPosts
+      .bind(to: tableView.rx.items(
+        cellIdentifier: CartPostSheetTableCell.identifier,
+        cellType: CartPostSheetTableCell.self)
+      ) { row, item, cell in
+        
+        cell.setData(post: item)
+      }
+      .disposed(by: disposeBag)
+  }
+}
---
+++ KeyCat/Presentation/Main/TabBar/Shopping/CartPost/Cell/CartPostSheetTableCell.swift
@@ -0,0 +1,72 @@
+//
+//  CartPostSheetTableCell.swift
+//  KeyCat
+//
+//  Created by 원태영 on 5/8/24.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+final class CartPostSheetTableCell: RxBaseTableViewCell {
+  
+  // MARK: - UI
+  private let containerView = UIView()
+  private let productImageView = UIImageView().configured {
+    $0.contentMode = .scaleAspectFill
+    $0.clipsToBounds = true
+    $0.layer.configure {
+      $0.cornerRadius = 10
+      $0.borderColor = KCAsset.Color.lightGrayBackground.color.cgColor
+      $0.borderWidth = 1
+    }
+  }
+  private let titleLabel = KCLabel(font: .medium(size: 14), line: 2)
+  private let priceView = ProductPriceView()
+  
+  // MARK: - Life Cycle
+  override func setHierarchy() {
+    
+    contentView.addSubviews(containerView)
+    
+    containerView.addSubviews(
+      productImageView,
+      titleLabel,
+      priceView
+    )
+  }
+  
+  override func setConstraint() {
+    
+    containerView.snp.makeConstraints { make in
+      make.edges.equalToSuperview().inset(20)
+    }
+    
+    productImageView.snp.makeConstraints { make in
+      make.top.equalToSuperview()
+      make.leading.equalToSuperview()
+      make.size.equalTo(80)
+    }
+    
+    titleLabel.snp.makeConstraints { make in
+      make.top.equalToSuperview()
+      make.leading.equalTo(productImageView.snp.trailing).offset(20)
+      make.trailing.equalToSuperview()
+    }
+    
+    priceView.snp.makeConstraints { make in
+      make.top.equalTo(titleLabel.snp.bottom).offset(10)
+      make.horizontalEdges.equalTo(titleLabel)
+      make.bottom.equalToSuperview()
+    }
+  }
+  
+  func setData(post: CommercialPost) {
+    
+    productImageView.load(with: post.mainImageURL)
+    titleLabel.text = post.title
+    priceView.productPrice = post.price
+  }
+}
