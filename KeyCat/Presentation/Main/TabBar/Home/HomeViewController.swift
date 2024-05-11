91d588017e4fad0a8563cb73d971b800cda035a5
Taeyoung Won <hexter1994@gmail.com>
2024-05-11 16:57:18 +0900

    add: HomeViewController 추가, HomeView 호스팅 연결

---
+++ KeyCat/Presentation/Main/TabBar/Home/HomeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  HomeViewController.swift
+//  KeyCat
+//
+//  Created by 원태영 on 5/10/24.
+//
+
+import SwiftUI
+import SnapKit
+import RxSwift
+import RxCocoa
+
+final class HomeViewController: RxBaseViewController {
+  
+  // MARK: - UI
+  private let homeViewHostingController = UIHostingController(rootView: HomeView())
+  
+  // MARK: - Life Cycle
+  override func setHierarchy() {
+    view.addSubview(homeViewHostingController.view)
+  }
+  
+  override func setConstraint() {
+    homeViewHostingController.view.snp.makeConstraints { make in
+      make.edges.equalTo(view.safeAreaLayoutGuide)
+    }
+  }
+  
+  override func setAttribute() {
+    
+  }
+  
+  override func bind() {
+    
+  }
+}
