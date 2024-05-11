37db90e2f6975f2e1bfccac22ebacbbf66d1dee6
Taeyoung Won <hexter1994@gmail.com>
2024-05-11 16:56:43 +0900

    add: HomeView 추가

---
+++ KeyCat/Presentation/Main/TabBar/Home/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  KeyCat
+//
+//  Created by 원태영 on 5/10/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+  
+  @StateObject private var viewModel = HomeViewModel()
+  
+  var body: some View {
+    
+    ScrollView {
+      
+    }
+  }
+}
