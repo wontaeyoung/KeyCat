5aa08eae5b2821365e066861b455f6acd275a3b1
Taeyoung Won <hexter1994@gmail.com>
2024-05-20 00:18:52 +0900

    #17 fix: 옵셔널 디코딩 예외처리 로직 추가

--- KeyCat/Data/DTO/ChatDTO.swift
+++ KeyCat/Data/DTO/ChatDTO.swift
@@ -14,6 +14,41 @@ struct ChatDTO: DTO, DefaultValueProvidable {
   let sender: UserDTO
   let files: [String]
   
+  enum CodingKeys: CodingKey {
+    case chat_id
+    case room_id
+    case content
+    case createdAt
+    case sender
+    case files
+  }
+  
+  init(from decoder: any Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    self.chat_id = try container.decode(String.self, forKey: .chat_id)
+    self.room_id = try container.decode(String.self, forKey: .room_id)
+    self.content = try container.decodeWithDefaultValue(String.self, forKey: .content)
+    self.createdAt = try container.decode(String.self, forKey: .createdAt)
+    self.sender = try container.decode(UserDTO.self, forKey: .sender)
+    self.files = try container.decode([String].self, forKey: .files)
+  }
+  
+  init(
+    chat_id: String,
+    room_id: String,
+    content: String,
+    createdAt: String,
+    sender: UserDTO,
+    files: [String]
+  ) {
+    self.chat_id = chat_id
+    self.room_id = room_id
+    self.content = content
+    self.createdAt = createdAt
+    self.sender = sender
+    self.files = files
+  }
+  
   static var defaultValue: ChatDTO {
     return ChatDTO(
       chat_id: .defaultValue,
