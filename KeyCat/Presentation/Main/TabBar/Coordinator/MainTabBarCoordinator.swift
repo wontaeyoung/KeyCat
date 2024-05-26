8bb7aced5bd0ba07ea5775126b0b176173068f4f
Taeyoung Won <hexter1994@gmail.com>
2024-05-26 23:54:00 +0900

    #17 fix: Chat 탭 추가, Coordinator 설정 로직 재사용 함수 적용

--- KeyCat/Presentation/Main/TabBar/Coordinator/MainTabBarCoordinator.swift
+++ KeyCat/Presentation/Main/TabBar/Coordinator/MainTabBarCoordinator.swift
@@ -17,7 +17,7 @@ extension MainTabBarCoordinator: TabBarDelegate {
   }
 }
 
-final class MainTabBarCoordinator: SubCoordinator {
+final class MainTabBarCoordinator: Coordinator {
   
   // MARK: - Property
   var navigationController: UINavigationController
@@ -49,35 +49,36 @@ final class MainTabBarCoordinator: SubCoordinator {
   }
   
   private func makeNavigationController(with page: MainTabBarPage) -> UINavigationController {
-    return UINavigationController().configured {
-      $0.tabBarItem = page.tabBarItem
-      connectTabFlow(page: page, tabPageController: $0)
-    }
+    return UINavigationController().configured { connectTabFlow(page: page, tabPageController: $0) }
   }
   
   private func connectTabFlow(page: MainTabBarPage, tabPageController: UINavigationController) {
+    tabPageController.tabBarItem = page.tabBarItem
+    let coordinator = makeCoordinator(page: page, tabPageController: tabPageController)
+    connectCoordinator(coordinator)
+  }
+  
+  private func makeCoordinator(page: MainTabBarPage, tabPageController: UINavigationController) -> any SubCoordinator {
     switch page {
       case .home:
-        let coordinator = HomeCoordinator(tabPageController)
-        addChild(coordinator)
-        coordinator.start()
-        coordinator.tabBarDelegate = self
-        coordinator.signOutDelegate = self
+        return HomeCoordinator(tabPageController)
         
       case .shopping:
-        let coordinator = ShoppingCoordinator(tabPageController)
-        addChild(coordinator)
-        coordinator.start()
-        coordinator.tabBarDelegate = self
-        coordinator.signOutDelegate = self
+        return ShoppingCoordinator(tabPageController)
+        
+      case .chat:
+        return ChatCoordinator(tabPageController)
         
       case .profile:
-        let coordinator = MyPageCoordinator(tabPageController)
-        addChild(coordinator)
-        coordinator.start()
-        coordinator.tabBarDelegate = self
-        coordinator.signOutDelegate = self
+        return MyPageCoordinator(tabPageController)
     }
+  }
+  
+  private func connectCoordinator(_ coordinator: any SubCoordinator) {
+    addChild(coordinator)
+    coordinator.start()
+    coordinator.tabBarDelegate = self
+    coordinator.signOutDelegate = self
   }
 }
 
