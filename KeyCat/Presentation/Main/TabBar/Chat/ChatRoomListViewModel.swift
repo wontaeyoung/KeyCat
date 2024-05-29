5c65d007e7cf70e584d1ce213658b21a177028b0
Taeyoung Won <hexter1994@gmail.com>
2024-05-29 20:16:22 +0900

    #17 feat: 뷰 로드 이벤트와 채팅방 조회 기능 연결

--- KeyCat/Presentation/Main/TabBar/Chat/ChatRoomListViewModel.swift
+++ KeyCat/Presentation/Main/TabBar/Chat/ChatRoomListViewModel.swift
@@ -12,25 +12,47 @@ final class ChatRoomListViewModel: ViewModel {
   
   // MARK: - I / O
   struct Input {
+    let viewDidLoadEvent: PublishRelay<Void>
     
+    init(
+      viewDidLoadEvent: PublishRelay<Void> = .init()
+    ) {
+      self.viewDidLoadEvent = viewDidLoadEvent
+    }
   }
   
   struct Output {
-    
+    let chatRooms: Driver<[ChatRoom]>
   }
   
   // MARK: - Property
   let disposeBag = DisposeBag()
   weak var coordinator: ChatCoordinator?
+  private let chatRoomUsecase: ChatRoomUsecase
   
   // MARK: - Initializer
-  init() {
-    
+  init(chatRoomUsecase: ChatRoomUsecase = ChatRoomUsecaseImpl()) {
+    self.chatRoomUsecase = chatRoomUsecase
   }
   
   // MARK: - Method
   func transform(input: Input) -> Output {
     
-    return Output()
+    let chatRooms = BehaviorRelay<[ChatRoom]>(value: [])
+    
+    /// 뷰 로드 > 채팅방 리스트 요청
+    input.viewDidLoadEvent
+      .withUnretained(self)
+      .flatMap { owner, _ in
+        return owner.chatRoomUsecase.fetchMyChatRooms()
+          .catch {
+            owner.coordinator?.showErrorAlert(error: $0)
+            return .never()
+          }
+      }
+      .bind(to: chatRooms)
+      .disposed(by: disposeBag)
+    
+    return Output(chatRooms: chatRooms.asDriver())
   }
 }
