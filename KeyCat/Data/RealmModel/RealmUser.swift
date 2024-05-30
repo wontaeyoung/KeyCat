f6d0fcb4ed2b3d5ae275e05f78d2ce25d25aa658
Taeyoung Won <hexter1994@gmail.com>
2024-05-30 23:30:54 +0900

    #17 add: Realm User 모델 정의

---
+++ KeyCat/Data/RealmModel/RealmUser.swift
@@ -0,0 +1,31 @@
+//
+//  RealmUser.swift
+//  KeyCat
+//
+//  Created by 원태영 on 5/30/24.
+//
+
+import KazRealm
+import RealmSwift
+
+final class RealmUser: Object, RealmModel {
+  
+  enum Column: String {
+    
+    case userID
+    case nickname
+    case profileImageURLString
+  }
+  
+  @Persisted(primaryKey: true) var userID: String
+  @Persisted var nickname: String
+  @Persisted var profileImageURLString: String
+  
+  convenience init(userID: String, nickname: String, profileImageURLString: String) {
+    self.init()
+    
+    self.userID = userID
+    self.nickname = nickname
+    self.profileImageURLString = profileImageURLString
+  }
+}
